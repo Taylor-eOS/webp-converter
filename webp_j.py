4431e622e966e09553ae2ae24577a5bcf76fab1a
Taylor-eOS <taylor.eos@e.email>
2024-05-01 23:06:42 +0200

    Now moves WEBP files to a folder

--- webp_j.py
+++ webp_j.py
@@ -8,6 +8,10 @@ def convert_webp_to_png(directory):
     # Expand the user's home directory
     directory = os.path.expanduser(directory)
 
+    webp_directory = os.path.join(directory, "webp_files")
+    if not os.path.exists(webp_directory):
+        os.makedirs(webp_directory)
+
     # Loop through all files in the directory
     for filename in os.listdir(directory):
         if filename.endswith(".webp"):
@@ -21,12 +25,13 @@ def convert_webp_to_png(directory):
                 new_filename = filename[:-4] + "jpg"
                 new_file_path = os.path.join(directory, new_filename)
                 rgb_img.save(new_file_path, 'JPEG', quality=95)
-                print(f"Converted {filename} to JPEG")
             else:
                 new_filename = filename[:-4] + "jpg"
                 new_file_path = os.path.join(directory, new_filename)
                 img.save(new_file_path, 'JPEG', quality=95)
-                print(f"Converted {filename} to JPEG")
+            print(f"Converted {filename} to JPEG")
+            #os.remove(file_path)
+            os.rename(file_path, os.path.join(webp_directory, filename))
 
 # Specify the directory containing the WEBP files
 convert_webp_to_png(directory)
