582c5ae365134b4ddd85d10f21a393a2d67484b4
Taylor-eOS <taylor.eos@e.email>
2024-05-04 18:33:24 +0200

    Now doesn't change file order

--- webp_j.py
+++ webp_j.py
@@ -1,37 +1,31 @@
 import os
 from PIL import Image
+import time
 
 #Converts all WEBP files in the direcotry to PNG format
 directory = '~/Desktop'
 
 def convert_webp_to_png(directory):
-    # Expand the user's home directory
     directory = os.path.expanduser(directory)
 
     webp_directory = os.path.join(directory, "webp_files")
     if not os.path.exists(webp_directory):
         os.makedirs(webp_directory)
 
-    # Loop through all files in the directory
     for filename in os.listdir(directory):
         if filename.endswith(".webp"):
-            # Construct full file path
             file_path = os.path.join(directory, filename)
-            # Open the WEBP file with Pillow
-            img = Image.open(file_path)
-            #print(f"Convert {filename}")
+            new_filename = filename[:-4] + "jpeg"
+            new_file_path = os.path.join(directory, new_filename)
+            webp_filename = os.path.join(webp_directory, filename)
+            os.rename(file_path, webp_filename)
+            img = Image.open(webp_filename)
             if img.mode == 'RGBA':
-                rgb_img = img.convert('RGB')  # Convert to RGB, dropping the alpha channel
-                new_filename = filename[:-4] + "jpeg"
-                new_file_path = os.path.join(directory, new_filename)
-                rgb_img.save(new_file_path, 'JPEG', quality=95)
-            else:
-                new_filename = filename[:-4] + "jpeg"
-                new_file_path = os.path.join(directory, new_filename)
-                img.save(new_file_path, 'JPEG', quality=95)
+                img = img.convert('RGB')
+            img.save(new_file_path, 'JPEG', quality=95)
             print(f"Converted {filename} to JPEG")
             #os.remove(file_path)
-            os.rename(file_path, os.path.join(webp_directory, filename))
+            time.sleep(0.3)
 
 # Specify the directory containing the WEBP files
 convert_webp_to_png(directory)
